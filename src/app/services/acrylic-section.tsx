def27ba24ce7906a066fbdd5048946681cc3747d
Ton Huynh <kintonhtt@gmail.com>
2025-05-15 20:39:07 -0700

    test

--- src/app/services/acrylic-section.tsx
+++ src/app/services/acrylic-section.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { acrylic } from "@/constants/categories/acrylic";
-import { motion } from "motion/react";
 import { CornerPill } from "./reusables/corner-pill";
 import { ServiceAddOns } from "./reusables/service-addons";
 import { ServiceHeading } from "./reusables/service-heading";
@@ -10,11 +9,11 @@ export function AcrylicSection() {
   // const [showMore, setShowMore] = useState(false);
 
   return (
-    <motion.section
-      initial={{ opacity: 0, y: 40 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
-      viewport={{ once: true }}
+    <section
+      // initial={{ opacity: 0, y: 40 }}
+      // whileInView={{ opacity: 1, y: 0 }}
+      // transition={{ duration: 0.6, ease: "easeOut" }}
+      // viewport={{ once: true }}
       className="mb-8"
     >
       <section className="border-claybrown bg-warmsand group relative flex flex-col gap-2 overflow-hidden rounded-tl-md rounded-tr-3xl rounded-br-md rounded-bl-3xl border-t border-b border-dashed shadow-sm transition-all duration-300 hover:-translate-y-1 hover:shadow-xl">
@@ -67,6 +66,6 @@ export function AcrylicSection() {
 
         <CornerPill position="left" color="claybrown" />
       </section>
-    </motion.section>
+    </section>
   );
 }
