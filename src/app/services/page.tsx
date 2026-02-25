f8a82cc385c28329685bfd1fa53279cd5ecab333
Ton Huynh <kintonhtt@gmail.com>
2026-02-24 18:11:17 -0800

    uncomment maintenance section for dev

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -5,9 +5,17 @@ import { Metadata } from "next";
 // import { DipPowderSection } from "./dip-powder-section";
 // import { GelManicureSection } from "./gel-manicure-section";
 // import { GelXSection } from "./gelX-section";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { AcrylicSection } from "./acrylic-section";
+import { BasicManicureSection } from "./basic-manicure-section";
+import { BiabGelSection } from "./biab-gel-section";
+import { DipPowderSection } from "./dip-powder-section";
+import { GelManicureSection } from "./gel-manicure-section";
+import { GelXSection } from "./gelX-section";
 import { ServicesHeader } from "./hero";
+import { KidsSection } from "./kids-section";
+import { PedicureSection } from "./pedicure-section";
+import { RemovalSection } from "./removal-section";
+import { WaxingSection } from "./waxing-section";
 // import { KidsSection } from "./kids-section";
 // import { PedicureSection } from "./pedicure-section";
 // import { RemovalSection } from "./removal-section";
@@ -25,7 +33,7 @@ export default function Page() {
       <ServicesHeader />
 
       {/* Maintenance Section - please check back later */}
-      <section className="mx-auto my-16 flex w-full max-w-(--breakpoint-2xl) flex-col gap-16 px-4 text-center sm:px-8">
+      {/* <section className="mx-auto my-16 flex w-full max-w-(--breakpoint-2xl) flex-col gap-16 px-4 text-center sm:px-8">
         <h2 className="text-xl tracking-widest uppercase md:text-3xl">Maintenance</h2>
 
         <div className="flex flex-col items-center gap-4">
@@ -36,16 +44,15 @@ export default function Page() {
           <p>
             For our current services, please visit our Fresha page by clicking the button below.
           </p>
-          {/* https://www.fresha.com/a/shape-nails-medford-medford-243-boston-avenue-iealt7lq?pId=2485107 */}
           <Button asChild className="h-12 self-center text-base tracking-wide">
             <Link href="https://www.fresha.com/a/shape-nails-medford-medford-243-boston-avenue-iealt7lq?pId=2485107">
               View services on Fresha
             </Link>
           </Button>
         </div>
-      </section>
+      </section> */}
 
-      {/* <main className="mx-auto my-16 flex w-full max-w-(--breakpoint-2xl) flex-col gap-16 px-4 sm:px-8">
+      <main className="mx-auto my-16 flex w-full max-w-(--breakpoint-2xl) flex-col gap-16 px-4 sm:px-8">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:items-start">
           <div className="flex flex-col gap-8">
             <BasicManicureSection />
@@ -62,7 +69,7 @@ export default function Page() {
             <WaxingSection />
           </div>
         </div>
-      </main> */}
+      </main>
     </>
   );
 }
